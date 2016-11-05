170b36057cdf45c4c95c6ec6690395cbbf59674d
Amod Pandey <amodpandey@gmail.com>
2016-11-05 19:40:45 +0000

    reader

---
+++ js/reader.js
@@ -0,0 +1,23 @@
+function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+function(callback) {
+  $.getJSON('https://testnet.api.coinprism.com/v1/addresses/2NA5TP5HR9LjLzPeG985EGMH6AMyLHH49u2/transactions', function(data) {
+  	console.log(data);
+    $.each(data, function( index, value ) {
+      var output = value.outputs[1]
+      var ret = []
+      if(output && output.script.match('^6a')){
+        var ascii = hex2a(output.script.substring(2));
+        console.log( index + ": " +  ascii);   
+        ret.push(ascii)
+      }        
+      callback(ret);
+    });
+  });
+}
