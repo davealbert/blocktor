4799338b69ed3c51209768307c59b7f2e6f966eb
Diego Furtado <diego.furtado.souza@teamaol.com>
2016-11-05 22:48:13 +0000

    last changes

--- js/transactions.js
+++ js/transactions.js
@@ -1,41 +1,51 @@
 var bitcoin = require("bitcoinjs-lib")
-var request = require('request');
+var request = require('request')
 
 var network = bitcoin.networks.testnet
 var tx = new bitcoin.TransactionBuilder(network)
 
+var sourceWallet = "mkEEZ8gqfJsH1MRsQa9tBs1c66o561952Y"
+
 request({
-  url: "http://tbtc.blockr.io/api/v1/address/info/mzXgPXJvGB1VgWR12fjekhS5Fsu8p9ffq7",
+  url: "http://tbtc.blockr.io/api/v1/address/info/" + sourceWallet,
   method: "GET",
   headers: {
     'Content-Type': 'application/json'
-  },
+  }
 }, function optionalCallback(error, response, body){
   var lastTx = JSON.parse(body).data.last_tx.tx;
   console.log("last tx: " + lastTx)
-  buildNewTransaction(lastTx)
+  request({
+    url: "https://testnet.api.coinprism.com/v1/transactions/" + lastTx,
+    method: "GET",
+    strictSSL: false,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, function optionalCallback(error, response, body){
+    var transactions = JSON.parse(body).outputs;
+    var txIndex = transactions[transactions.length - 1].index;
+    console.log("last tx idx: " + txIndex)
+    buildNewTransaction(lastTx, txIndex)
+  })
 })
 
-function buildNewTransaction(txId){
-  tx.addInput(txId, 0)
+function buildNewTransaction(txId, txIndex){
+  tx.addInput(txId, 1)
 
   // Add the output (who to pay to):
   // [payee's address, amount in satoshis]
-  tx.addOutput("2Mwf9wKZvib1b96PoePgjZ4ZjRrKLhjKBvK", 2233)
+  tx.addOutput("muEqoXnoWbYarzusdTKeHduXda3ksMCotK", 1000)
 
-  var data = new Buffer("[start]id:af880261e91629de48baf8bcad8abe19aa1bee34|name:Blockchain-Hackathon.png[end]")
-   data.write("[start]id:af880261e91629de48baf8bcad8abe19aa1bee34|", 'utf-8')
-   data.write("desc: This is an image ", 'utf-8')
-   data.write("from the blockchain-hackathon.com ", 'utf-8')
-   data.write("website[end] ", 'utf-8')
-
+  var data = new Buffer("[start]id:af880261e91629de48baf8bcad8abe19aa1bee34|name:hahahahaha[end]")
+   
   var ret = bitcoin.script.compile([
       bitcoin.opcodes.OP_RETURN, 
       data
     ])
   tx.addOutput(ret, 0);
   // Initialize a private key using WIF
-  var privateKeyWIF = 'cT8M1UT7wJCrKUmAeqw9LFEMj6zjnRsDWU75kjd2ABNPCa4VqsvS'
+  var privateKeyWIF = 'cV7mTRkSbMNsE8zURVYf3FJPczWxCipibGvtZyh4SAwJ9Jrq28Dz'
   var keyPair = bitcoin.ECPair.fromWIF(privateKeyWIF, network)
 
   // Sign the first input with the new key
