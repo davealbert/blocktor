7e97a8bb0318a6d85d98ad4b5ec1451c38419c07
Dave Albert <dave.albert@gmail.com>
2016-11-05 17:01:19 +0000

    MockTransactions

--- html/main.js
+++ html/main.js
@@ -1,9 +1,61 @@
+var MockTransactions = [
+   { "id" : 1, "metadata" : "[start]this is some " },
+   { "id" : 2, "metadata" : "text that spans " },
+   { "id" : 3, "metadata" : "multiple transactions[end]" },
+   { "id" : 4, "metadata" : "[start]this is MORE " },
+   { "id" : 5, "metadata" : "text that also spans " },
+   { "id" : 6, "metadata" : "multiple transactions[end]" }
+];
+
 /**
  * Jquery functions as and if we need them.
  */
 $( document ).ready(function() {
-   $('#main').html('hello world');
+   $('#main').html('');
+   //var url = 'https://blockchain.info/address/17sknTxzAMZnUSajqtN8MUAmjgRK7vTZms?format=json&cors=true';
+   var url = 'https://testnet.api.coinprism.com/v1/addresses/2N3dKfnUquvThKoRcxqsHsisV3dqTVjvCEp/transactions';
+
+   $.ajax({
+      url: url,
+      crossDomain: true
+   }).done(function(data) {
+      console.log(data);
+      parseTransacrions(MockTransactions);
+   });
 });
+
+
+function parseTransacrions(transactions) {
+   var buffer;
+   for (var i = 0; i < transactions.length; i++) {
+      var transaction = transactions[i];
+
+      // Start
+      if (transaction.metadata.indexOf('[start]') !== -1) {
+         buffer = [];
+         transaction.metadata = transaction.metadata.replace(/\[start\]/g,'');
+      }
+
+      // Not End
+      if (transaction.metadata.indexOf('[end]') === -1) {
+         buffer.push(transaction.metadata);
+      }
+
+      // End
+      if (transaction.metadata.indexOf('[end]') !== -1) {
+         buffer.push(transaction.metadata.replace(/\[end\]/g,''));
+         processContent(buffer.join(''));
+      }
+   };
+}
+
+
+/**
+ * Take buffer built from parts and transform to useable content.
+ */
+function processContent(contentBuffer) {
+   $('#main').append(contentBuffer + "<br>");
+}
 
 
 /**
