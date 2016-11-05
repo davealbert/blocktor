f8bbabceac951eb51411d3a71d4cc7e31a3d45c6
Dave Albert <dave.albert@gmail.com>
2016-11-05 09:13:01 +0000

    Added Angular 1

--- html/main.js
+++ html/main.js
@@ -4,3 +4,15 @@
 $( document ).ready(function() {
    $('#main').html('hello world');
 });
+
+
+/**
+ * Angular 1 functions/methods
+ *   We are using Angular 1 over 2 because Agular 2 requires a http server
+ *   we want this application to function locally if possible.
+ */
+angular.module('blockTorApp', [])
+.controller('BlockTorController', function() {
+   var blockTor = this;
+   blockTor.name = "We are Team BlockTor";
+});
