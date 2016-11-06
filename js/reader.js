edaca39ce5a5b3c354addd131d3c46d7e5c5be9d
Amod Pandey <amodpandey@gmail.com>
2016-11-06 06:44:07 +0000

    listing the metadata from a different API

--- js/reader.js
+++ js/reader.js
@@ -1,3 +1,5 @@
+var receiverAddress = 'muEqoXnoWbYarzusdTKeHduXda3ksMCotK';
+
 function hex2a(hexx) {
     var hex = hexx.toString();//force conversion
     var str = '';
@@ -6,18 +8,48 @@ function hex2a(hexx) {
     return str;
 }
 
+function readBatch(txns, ret) {
+  console.log(txns.length)
+  BATCH_SIZE = 10
+  var size = txns.length
+  var iteration = 0;
+  while(size >= 0) {
+    var start = iteration * BATCH_SIZE
+    var txnsStr = txns.slice(start, size > BATCH_SIZE ? start + BATCH_SIZE : txns.length).join(',')
+    console.log((iteration * BATCH_SIZE)+" "+(size > BATCH_SIZE ? BATCH_SIZE : size))
+    size -= BATCH_SIZE;
+    iteration++;
+    console.log("https://tbtc.blockr.io/api/v1/tx/info/"+txnsStr)
+    $.getJSON("https://tbtc.blockr.io/api/v1/tx/info/"+txnsStr, function(data) {
+      $.each(data.data, function(index, value) {
+        $.each(value.vouts, function(i, v) {
+          //console.log(v)
+          if(v.address == "NONSTANDARD") {
+            var ascii = hex2a(v.extras.asm)
+            console.log( index + ": " +  ascii);   
+            ret.push(ascii)
+          }
+        })
+      })
+    })
+  }
+}
+
 function getTorrentList(callback) {
-  $.getJSON('https://testnet.api.coinprism.com/v1/addresses/2NA5TP5HR9LjLzPeG985EGMH6AMyLHH49u2/transactions', function(data) {
-  	console.log(data);
-    $.each(data, function( index, value ) {
-      var output = value.outputs[1]
-      var ret = []
-      if(output && output.script.match('^6a')){
-        var ascii = hex2a(output.script.substring(2));
-        console.log( index + ": " +  ascii);   
-        ret.push(ascii)
-      }        
-      callback(ret);
+  $.getJSON('https://tbtc.blockr.io/api/v1/address/txs/'+receiverAddress, function(data) {
+    console.log(data);
+    var txns = []
+    $.each(data.data.txs, function(index, value) {
+      txns.push(value.tx)
     });
+
+    data = []
+    readBatch(txns, data)
+    callback(data);
   });
 }
+
+function x(data) {
+  console.log(data)
+}
+getTorrentList(x)
